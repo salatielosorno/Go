61860d5882fac18c336bb6769e06ba7cdaaf48c9
salatielosorno <salatiel.linares@gmail.com>
2024-06-01 13:22:37 -0600

    feat(app): variables

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	variables.MuestroEnteros()
+	variables.RestoVariables()
 }
---
+++ variables/resto.go
@@ -0,0 +1,23 @@
+package variables
+
+import (
+	"fmt"
+	"time" /* we need to explicit imported it */
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+}
